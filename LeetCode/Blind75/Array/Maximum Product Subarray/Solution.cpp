945cf1195b357ead430b89ac7283972a2698e890
adithyar262 <39980616+adithyar262@users.noreply.github.com>
2023-12-30 17:45:04 -0500

    Update and rename LeetCode/Blind75/6/Solution.cpp to LeetCode/Blind75/Array/Maximum Product Subarray/Solution.cpp

--- LeetCode/Blind75/Array/Maximum Product Subarray/Solution.cpp
+++ LeetCode/Blind75/Array/Maximum Product Subarray/Solution.cpp
@@ -1,6 +1,6 @@
 /*
 152. Maximum Product Subarray
-
+https://leetcode.com/problems/maximum-product-subarray/
 Given an integer array nums, find a 
 subarray
  that has the largest product, and return the product.
