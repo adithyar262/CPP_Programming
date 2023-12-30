8bca5f63e61861be93742ee54423503f3dfbe1ef
adithyar262 <39980616+adithyar262@users.noreply.github.com>
2023-12-30 15:14:25 -0500

    Update Solution.cpp

--- LeetCode/Blind75/7/Solution.cpp
+++ LeetCode/Blind75/7/Solution.cpp
@@ -1,60 +1,136 @@
 /*
-153. Find Minimum in Rotated Sorted Array
+33. Search in Rotated Sorted Array
 
-Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+There is an integer array nums sorted in ascending order (with distinct values).
 
-[4,5,6,7,0,1,2] if it was rotated 4 times.
-[0,1,2,4,5,6,7] if it was rotated 7 times.
-Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
 
-Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 
-You must write an algorithm that runs in O(log n) time.
+You must write an algorithm with O(log n) runtime complexity.
 
  
 
 Example 1:
 
-Input: nums = [3,4,5,1,2]
-Output: 1
-Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
 Example 2:
 
-Input: nums = [4,5,6,7,0,1,2]
-Output: 0
-Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
 Example 3:
 
-Input: nums = [11,13,15,17]
-Output: 11
-Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+Input: nums = [1], target = 0
+Output: -1
  
 
 Constraints:
 
-n == nums.length
-1 <= n <= 5000
--5000 <= nums[i] <= 5000
-All the integers of nums are unique.
-nums is sorted and rotated between 1 and n times.
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+All values of nums are unique.
+nums is an ascending array that is possibly rotated.
+-104 <= target <= 104
 */
 
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-        int arrSize = nums.size(), startIdx = 0;
-        int start = nums[0], mid = nums[arrSize / 2], end = nums[arrSize - 1];
-        while(start > end) {
-            startIdx += ((arrSize - startIdx) / 2);
-            mid = nums[startIdx];
-            if(nums[startIdx] > end) {
-                start = nums[startIdx + 1];
+    int search(vector<int>& nums, int target) {
+        int arrSize = nums.size();
+        if(arrSize == 1) {
+            if (nums[0] == target)
+                return 0;
+            else
+                return -1;
+        }
+        int start = 0, end = nums.size() - 1;
+        while(start < end) {
+            int mid = start + ((end - start) / 2);
+            if(nums[mid] > nums[start]) { 
+                if(nums[mid] < nums[end]) { // 4 5 6 7 8 9 10
+                    if(nums[mid] > target) { // target = 4, 5, 6
+                        if(target > nums[start]) { // target = 5, 6
+                            end = mid;
+                        } else if (target == nums[start]) { // target = 4
+                            return start;
+                        } else { // target = 3
+                            return -1;
+                        }
+                    } else if(nums[mid] < target) { // target = 8, 9, 10
+                        if(target < nums[end]) { // target = 8, 9
+                            start = mid + 1;
+                        } else if (target == nums[end]) {  // target = 10
+                            return end;
+                        } else {  // target = 11
+                            return -1;
+                        }
+                    } else { // target = 7
+                        return mid;
+                    }
+                } else { // 4 5 6 7 8 1 2
+                    if(nums[mid] > target) { // target = 1, 2, 4, 5, 6
+                        if(target > nums[end]) { // target = 4, 5, 6
+                            if(target > nums[start]) // target = 5, 6
+                                end = mid;
+                            else if(target == nums[start]) // target = 4
+                                return start;
+                            else // target = 3
+                                return -1;
+                        } else if (target < nums[end]) { // target = 1
+                            start = mid + 1;
+                        } else { // target = 2
+                            return end;
+                        }
+                    } else if(nums[mid] < target) { // target = 8
+                        if(target < nums[end]) { // target = 8, 9
+                            return -1;
+                        } else if (target == nums[end]) {  // target = 10
+                            return end;
+                        } else {  // target = 11
+                            start = mid + 1;
+                        }
+                    } else { // target = 7
+                        return mid;
+                    }
+                }
+            } else if(nums[mid] < nums[start]) { // 7 8 9 0 1 2 3
+                if(nums[mid] < nums[end]) { // 7 8 0 1 2 3 4
+                    if(nums[mid] > target) { // target = 0
+                        if(target > nums[start]) { // target = 5, 6
+                            return -1;
+                        } else if (target == nums[start]) { // target = 4
+                            return start;
+                        } else { // target = 3
+                            end = mid;
+                        }
+                    } else if(nums[mid] < target) { // target = 2, 3, 4
+                        if(target < nums[end]) { // target = 2, 3
+                            start = mid + 1;
+                        } else if (target == nums[end]) {  // target = 4
+                            return end;
+                        } else if (target < nums[start]) {  // target = 4
+                            return -1;
+                        } else if (target == nums[start]) {  // target = 4
+                            return start;
+                        } else {  // target = 11
+                            end = mid;
+                        }
+                    } else { // target = 7
+                        return mid;
+                    }
+                } else { // 4 5 6 7 8 1 2
+                    return -1;
+                }
             } else {
-                end = mid;
-                arrSize = startIdx;
-                startIdx = 0;
+                if(target == nums[start])
+                    return start;
+                else if(target == nums[end])
+                    return end;
+                else
+                    return -1;
             }
         }
-        return start;
+        return -1;
     }
 };
